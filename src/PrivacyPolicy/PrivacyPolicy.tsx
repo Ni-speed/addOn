9c7597f2d83ed3b9c57bab01adc4f3989a0dedca
Ni-speed <railmir@gmail.com>
2024-06-17 16:43:02 +0500

    fix2

--- src/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/PrivacyPolicy/PrivacyPolicy.tsx
@@ -9,14 +9,16 @@ export const PrivacyPolicy = () => {
         disclosure of Your information when You use the Service and tells You about Your privacy
         rights and how the law protects You. Intelsy use and transfer to any other app of
         information received from Google APIs will adhere to
+        <br />
         <a
           className={s.link}
           href={'https://developers.google.com/terms/api-services-user-data-policy'}
         >
           Google API Services User Data Policy
         </a>
-        , including the Limited Use requirements. We use Your Personal data to provide and improve
-        the Service. By using the Service, You agree to the collection and use of information in
+        <br />
+        including the Limited Use requirements. We use Your Personal data to provide and improve the
+        Service. By using the Service, You agree to the collection and use of information in
         accordance with this Privacy Policy. This Privacy Policy has been created with the help of
         the Free Privacy Policy Generator. Interpretation and Definitions Interpretation The words
         of which the initial letter is capitalized have meanings defined under the following
